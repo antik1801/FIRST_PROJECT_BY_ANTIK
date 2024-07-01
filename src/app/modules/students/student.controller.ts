cae905f362ffaef8f11ce27311a760e8b327a459
antik1801 <antik.edu@gmail.com>
2024-07-01 21:15:34 +0600

    students controller funcion added

--- src/app/modules/students/student.controller.ts
+++ src/app/modules/students/student.controller.ts
@@ -1,5 +1,18 @@
 import express,{Request, Response} from "express";
+import { StudentServices } from "./student.services";
 
-const createStudent = (req: Request, res: Response) =>{
+const createStudent =async (req: Request, res: Response) =>{
+   try {
     const student = req.body;
+    const result = await StudentServices.createStudentInDB(student);
+    
+    res.status(200).json({
+        success: true,
+        message: 'Student is created successfully',
+        data: result
+    })
+
+   } catch (error) {
+        console.log(error)
+   }
 }
