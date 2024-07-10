9b05fb0cfced81480447f8158a386f8dc7ec32d2
Gazi Ehsanul Haque <antik.edu@gmail.com>
2024-07-10 10:40:28 +0600

    user.interface.ts and user.model.ts is built

---
+++ src/app/modules/users/user.model.ts
@@ -0,0 +1,39 @@
+import { model, Schema } from "mongoose";
+import { TUser } from "./user.interface";
+
+
+const userSchema = new Schema<TUser>({
+    id: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    needsPasswordChange: {
+        type: Boolean,
+        default: true,
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'student', 'faculty']
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false,
+    },
+    status: {
+        type: String,
+        enum: ['in-progress', 'blocked'],
+        default: 'in-progress'
+    }
+},
+{
+    timestamps: true
+}
+)
+
+
+export const User = model<TUser>('Users', userSchema);
+
