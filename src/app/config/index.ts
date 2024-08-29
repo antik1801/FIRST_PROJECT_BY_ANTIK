058f60974b327957470f071b9f18dcfb601bbfbb
Gazi Ehsanul Haque <antik.edu@gmail.com>
2024-08-29 12:23:11 +0600

    seattle office day 12

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -8,4 +8,5 @@ export default {
   database_url: process.env.DB_URL,
   bcrypt_salt_round: process.env.BCRYPT_SALT_ROUNDS,
   default_password: process.env.DEFAULT_PASS,
+  NODE_ENV: process.env.NODE_ENV
 };
